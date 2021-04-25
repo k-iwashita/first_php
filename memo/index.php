8211c674afaaad4863a7c31f82eb9353b0d9a6f2
k-iwashita <k.iwashita.gude@gmail.com>
2021-04-25 22:56:07 +0900

    create memo system

--- memo/index.php
+++ memo/index.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../php/css/style.css" />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <title>PHP</title>
-  </head>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../php/css/style.css" />
 
-  <body>
-    <header>
-      <h1 class="font-weight-normal">PHP</h1>
-    </header>
+  <title>PHP</title>
+</head>
 
-    <main>
-      <?php
-        try {
-          $db = new PDO('mysql:dbname=my_db;host=localhost;carset=utf-8',
-          'root', 'root');
-        } catch(PDOException $e ) {
-          echo 'error' . $e -> getMessage();
-        }
-      ?>
-    </main>
-  </body>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <?php
+    try {
+      $db = new PDO(
+        'mysql:dbname=my_db;host=localhost;carset=utf-8',
+        'root',
+        'root'
+      );
+    } catch (PDOException $e) {
+      echo 'error' . $e->getMessage();
+    }
+
+    // $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="momo", price=210, keyword="kandume, pinku, amai"');
+    // echo $count . 'data';
+
+    // $records = $db -> query('select * from my_items');
+    // while( $record = $records-> fetch()) {
+    //   print($record["item_name"]);
+    // };
+
+    ?>
+  </main>
+</body>
+
 </html>
---
+++ memo/input_do.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../php/css/style.css" />
+
+  <title>PHP</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+
+  <main>
+    <pre>
+      <?php
+      try {
+        $db = new PDO('mysql:dbname=my_db;host=localhost;charset=utf8', 'root', 'root');
+
+        $db -> exec('insert into memos set memo="' . $_POST['memo'] . '", created_at=NOW()');
+      } catch(PDOException $e) {
+        echo 'error' . $e -> getMessage();
+      };
+      ?>
+    </pre>
+  </main>
+</body>
+
+</html>
